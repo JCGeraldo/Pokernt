ea19eb566c116d293c561c1beb7ded6f66feb6a0
JCGeraldo <163364975+JCGeraldo@users.noreply.github.com>
2024-06-18 23:17:14 +0000

    inicializarMazo y barajarMazo

--- Pokernt.c
+++ Pokernt.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 #include "tdas/extra.h"
 #include "tdas/list.h"
 #include "tdas/heap.h"
@@ -9,10 +10,34 @@
 #include "tdas/queue.h"
 
 typedef struct{
-  char numero;
-  char palo;
+  char numero; // 1 a 13
+  int palo;  // 0-3 (0 es Corazones, 1 es Diamantes, 2 es Tréboles, 3 es Picas)
   int puntaje;
 }Carta;
+
+void inicializarMazo(Carta *mazo) {
+    int index = 0;
+    for (int palo = 0; palo < 3; palo++) {
+        for (int valor = 1; valor <= 13; valor++) {
+            mazo[index].numero = valor;
+            mazo[index].palo = palo;
+            index++;
+        }
+    }
+}
+
+void barajarMazo(Carta *mazo, int tamano,Stack *mazoBarajado) {
+  srand(time(NULL));
+  for (int i = 0; i < tamano; i++) {
+      int j = rand() % tamano;
+      Carta temp = mazo[i];
+      mazo[i] = mazo[j];
+      mazo[j] = temp;
+  }
+  for (int i = 0; i < tamano; i++)
+    stack_push(mazoBarajado,mazo+i);
+}
+
 
 
 
